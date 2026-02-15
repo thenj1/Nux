289863e13f08fad138dbd8dae7a270371f0b5b65
Thiago Mecenas <thiagomecena@hotmail.com>
2026-02-15 12:04:49 -0300

    remove duplicate root files

--- src/users/dto/login-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, IsEmail } from "class-validator";
-
-export class LoginDto{
-    @IsEmail({}, { message: 'Senha ou email inválido' })
-    email: string
-
-    @IsString({ message: 'Senha ou email inválido' })
-    password: string
-}
